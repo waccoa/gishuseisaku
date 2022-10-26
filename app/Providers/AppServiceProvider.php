99173ff3bf5b90a148f78f31dc4f7bc1c483012f
waccoa <hominikal54@gmail.com>
2022-10-26 21:35:49 +0900

    1026

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,7 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Facades\Schema;
-
+use Illuminate\Support\Facades\App;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -26,7 +26,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
-        if(\App::environment(['production'])) {
+        if(App::environment(['production'])) {
            \URL::forseScheme('https');
         }
     }
