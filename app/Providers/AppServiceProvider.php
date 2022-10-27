6234d79dadd7f309e4de46abdb64d8ee5c0576b6
waccoa <hominikal54@gmail.com>
2022-10-27 12:47:49 +0900

    change

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,15 +26,11 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
-<<<<<<< HEAD
+
         if(\APP::environment(['production'])){
             \URL::force('https');
         }
-=======
-        // if(App::environment(['production'])) {
-        //    \URL::forseScheme('https');
-        // }
->>>>>>> b78e404d81a521078fe901c5410c3f3672060e10
+
     }
     
 }
